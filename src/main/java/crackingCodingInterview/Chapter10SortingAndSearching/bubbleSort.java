68ea95683c89f003318411a25f4942538da901d6
LongMacbook <zzsushiboyzz@gmail.com>
2019-06-20 17:27:53 +0100

    bubble sort

---
+++ src/main/java/crackingCodingInterview/Chapter10SortingAndSearching/bubbleSort.java
@@ -0,0 +1,34 @@
+package crackingCodingInterview.Chapter10SortingAndSearching;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        int n = arr.length;
+
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-1-i; j++) {
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    step++;
+                }
+                printarr(arr);
+            }
+
+        }
+        System.out.println(step);
+
+
+    }
+    private static int step = 0;
+    public static void printarr(int[] arr){
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+
+        }
+        System.out.println();
+
+    }
+}
