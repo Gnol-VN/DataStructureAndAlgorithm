6381b145d35becb5c80484290699c370d81f847d
LongMacbook <zzsushiboyzz@gmail.com>
2019-06-30 00:25:04 +0100

    fractional knapsack

--- src/main/java/crackingCodingInterview/Greedy/FractionalKnapsack.java
+++ src/main/java/crackingCodingInterview/Greedy/FractionalKnapsack.java
@@ -6,9 +6,9 @@ import java.util.Comparator;
 public class FractionalKnapsack {
     public static void main(String[] args) {
         int weight[] = {10, 40, 20, 30};
-        int value[] = {60, 40, 50, 120};
+        int value[] = {60, 40, 100, 120};
         int capacity = 50;
-        int knapSackValue= 0;
+        double knapSackValue= 0;
         int n = weight.length;
         Item[] itemList = new Item[n];
         for (int i = 0; i < n; i++) {
@@ -17,19 +17,25 @@ public class FractionalKnapsack {
         }
         //sort
         Arrays.sort(itemList, (o1, o2) -> {
-            if(o1.ratio > o2.ratio) return -1;
-            else return 1;
+            if(o1.ratio > o2.ratio) return 1;
+            else return -1;
         });
         //take from smallest ratio to highest, until cannot take more, then break it
-        for (int i = 0; i  < n; i++) {
+        for (int i = 0; i < n; i++) {
             if(capacity - itemList[i].weight >0 ){
                 //take
                 capacity = capacity - itemList[i].weight;
                 knapSackValue = knapSackValue + itemList[i].value;
             }else {
-                int brokenWeight =
+                //Break the product's weight to fit the remainder of the knapsack
+                //After breaking, add the corresponding value
+                double brokenValue = (Double.valueOf(capacity) / Double.valueOf(itemList[i].weight)) * itemList[i].value;
+                knapSackValue = knapSackValue + brokenValue;
+                capacity = 0;
+
             }
         }
+        System.out.println("Knapsack value: "+ knapSackValue);
     }
     static class Item{
         int weight;
@@ -38,7 +44,7 @@ public class FractionalKnapsack {
         public Item(int weight, int value) {
             this.weight = weight;
             this.value = value;
-            this.ratio = weight/value;
+            this.ratio = Double.valueOf(weight)/ Double.valueOf(value);
         }
     }
 }
