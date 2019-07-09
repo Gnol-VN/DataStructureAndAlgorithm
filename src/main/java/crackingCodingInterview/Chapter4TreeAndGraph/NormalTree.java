21f072d70f8f5c24b4fafde3ae8532c39cbb4ef2
LongMacbook <zzsushiboyzz@gmail.com>
2019-07-09 22:19:48 +0100

    tree

--- src/main/java/crackingCodingInterview/Chapter4TreeAndGraph/NormalTree.java
+++ src/main/java/crackingCodingInterview/Chapter4TreeAndGraph/NormalTree.java
@@ -112,7 +112,7 @@ public class NormalTree {
     public static void main(String[] args) {
         Node node = new Node(5);
         node.insert(3);
-//        node.insert(1);
+        node.insert(1);
         node.insert(2);
         node.insert(4);
         node.insert(7);
