3b7d04ea976a9ea43c66f73d380b6a1c31a5f2cc
LongMacbook <zzsushiboyzz@gmail.com>
2019-08-21 23:10:28 +0100

    string isPermutation

--- src/main/java/crackingCodingInterview/recursion/LengthOfNumber1.java
+++ src/main/java/crackingCodingInterview/recursion/LengthOfNumber1.java
@@ -1,4 +1,17 @@
 package crackingCodingInterview.recursion;
 
 public class LengthOfNumber1 {
+    public static void main(String[] args) {
+        int[] arr = {0,1,1,1,1,1,1,0,0};
+        int length = getLength(arr,1,0);
+        System.out.println(length);
+
+    }
+    static int getLength(int[] arr, int currentIndex, int currentLength){
+        if(arr[currentIndex] ==1){
+            currentLength++;
+            return getLength(arr, currentIndex + 1, currentLength);
+        }
+        return currentLength;
+    }
 }
