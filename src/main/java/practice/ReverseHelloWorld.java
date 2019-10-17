b5c2495389d027b94968e6289bbd3346b6e74c65
LongMacbook <zzsushiboyzz@gmail.com>
2019-10-17 21:37:56 +0100

    Ancestor

---
+++ src/main/java/practice/ReverseHelloWorld.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class ReverseHelloWorld {
+    public static void main(String[] args) {
+        String s = "Hello, world";
+        StringBuilder builder = new StringBuilder();
+        String[] arr = s.split(" ");
+        for (int i = arr.length-1; i >=0; i--) {
+            if(arr[i].endsWith(",")){
+                builder.append(",");
+                arr[i] = arr[i].substring(0, arr[i].length()-1);
+            }
+            builder.append(arr[i]);
+            builder.append(" ");
+        }
+        System.out.println(builder.toString().trim());
+    }
+}
