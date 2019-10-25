4cd07c3e34578e418f2254da06df08d9542b84c8
LongMacbook <zzsushiboyzz@gmail.com>
2019-10-25 01:43:53 +0100

    assessments from Guidewire and SIG

--- src/main/java/guidewire/Guidewire.java
+++ src/main/java/guidewire/Guidewire.java
@@ -43,6 +43,7 @@ public class Guidewire {
             else if(movieExt.contains(extension)) {
                 movieSize += Integer.valueOf(size);
             }
+
             else{
                 otherSize += Integer.valueOf(size);
             }
